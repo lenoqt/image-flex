e3e60b755a05c6d409106509dd9a33dca7a6d977
Gustavo Barrios <gustavoantonio51@gmail.com>
2023-08-17 05:13:16 +0000

    added mqtt

--- components/CameraCapture.tsx
+++ components/CameraCapture.tsx
@@ -1,11 +1,17 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
+import mqtt from "mqtt";
 
 const CameraCapture: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [capturedImage, setCaptureImage] = useState<string | null>(null);
   const [cameraStarted, setCameraStarted] = useState(false);
+  const [mqttClient, setMqttClient] = useState<mqtt.MqttClient | null>(null);
+  const [mqttConnected, setMqttConnected] = useState(false);
+
+  const mqttBrokerUrl = process.env.MQTT_BROKER_URL || "test.mosquitto.org";
+  const mqttTopic = process.env.MQTT_TOPIC || "image-topic";
 
   const startCamera = async () => {
     try {
@@ -20,6 +26,28 @@ const CameraCapture: React.FC = () => {
     }
   };
 
+  const connectMqtt = () => {
+    const client = mqtt.connect(mqttBrokerUrl);
+    client.on("connect", () => {
+      setMqttClient(client);
+      setMqttConnected(true);
+    });
+    client.on("close", () => {
+      setMqttConnected(false);
+    });
+  };
+
+  useEffect(() => {
+    if (mqttClient) {
+      mqttClient.on("connect", () => {
+        setMqttConnected(true);
+      });
+      mqttClient.on("close", () => {
+        setMqttConnected(false);
+      });
+    }
+  }, [mqttClient]);
+
   const captureImage = () => {
     const canvas = canvasRef.current;
     const context = canvas?.getContext("2d");
@@ -33,17 +61,33 @@ const CameraCapture: React.FC = () => {
       const imageDataUrl = canvas.toDataURL("image/png");
       setCaptureImage(imageDataUrl);
     }
+
+    if (mqttClient) {
+      mqttClient.end();
+      setMqttClient(null);
+      setMqttConnected(false);
+    }
+  };
+
+  const saveImage = () => {
+    if (capturedImage && mqttClient) {
+      connectMqtt();
+      mqttClient.publish(mqttTopic, capturedImage);
+    }
   };
 
   return (
     <div>
       <h1>Camera Capture Interface</h1>
       <div>
+        {mqttConnected
+          ? <div style={{ color: "green" }}>Connected to MQTT</div>
+          : <div style={{ color: "red" }}>Disconnected from MQTT</div>}
         {!cameraStarted && <button onClick={startCamera}>Start Camera</button>}
         {cameraStarted && (
           <>
             <button onClick={captureImage}>Capture Image</button>
-            <p>Click Capture Image when ready</p>
+            <button onClick={saveImage} disabled={!capturedImage}>Save</button>
           </>
         )}
       </div>
@@ -53,8 +97,8 @@ const CameraCapture: React.FC = () => {
             <Image
               src={capturedImage}
               alt="Captured"
-              width={100}
-              height={100}
+              width={250}
+              height={250}
             />
             <a href={capturedImage} download="captured_image.png">
               Download Captured Image
@@ -67,6 +111,7 @@ const CameraCapture: React.FC = () => {
         style={{ display: cameraStarted ? "block" : "none" }}
         autoPlay
         muted
+        playsInline
       />
       <canvas ref={canvasRef} style={{ display: "none" }} />
     </div>
